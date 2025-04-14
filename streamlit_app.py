b2b8386a165f2a66ef64fd07f20535889d349996
Alvaro Marin <alvaro.marin.arancibia@gmail.com>
2025-04-14 12:24:09 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@
 import streamlit as st
 from snowflake.snowpark.functions import col
 import requests
-import pandas as pd
+import pandas as pd_df
 
 # Write directly to the app
 st.title("🥤Customize Your Smoothie!🥤")
